c8f8bbb17d443bee6fcd387a960c37009f39643e
Axel Asp <solidtop.asp@gmail.com>
2023-11-20 13:32:22 +0100

    initial commit

---
+++ src/main/java/com/example/geospot/GeoSpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.geospot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geospot/GeoSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geospot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
